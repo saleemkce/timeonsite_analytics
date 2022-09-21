e73d796b793236acf5304c5339792b8ef8a25f03
Saleem Khan <saleemkce@gmail.com>
2022-09-21 12:46:48 +0530

    Updated function code compatibilty across MariaDB and MySQL versions

--- tos_get_duration.sql
+++ tos_get_duration.sql
@@ -3,15 +3,25 @@
  *
  * Given time unit in seconds, it converts to more readable format
  *
- * Eg. TOS_GET_DURATION(72) => 00d 0:1:12
+ * Eg. SELECT TOS_GET_DURATION(72) => 00d 0:1:12
  */
 
+
+/**
+ * MariaDB version(s)
+ */
 DROP FUNCTION IF EXISTS TOS_GET_DURATION;
-DELIMITER $$
 CREATE FUNCTION TOS_GET_DURATION(seconds INT) 
-  RETURNS VARCHAR(16) 
-BEGIN
+  RETURNS VARCHAR(16)
   RETURN CONCAT(LPAD(FLOOR(HOUR(SEC_TO_TIME(seconds)) / 24), 2, 0), ' days ',TIME_FORMAT(SEC_TO_TIME(seconds % (24 * 3600)), '%H:%i:%s'));
-END; 
-$$
-DELIMITER;
+
+
+/**
+ * MySQL version(s)
+ * requires "DETERMINISTIC" keyword in recent versions.
+ */
+DROP FUNCTION IF EXISTS TOS_GET_DURATION;
+CREATE FUNCTION TOS_GET_DURATION(seconds INT) 
+  RETURNS VARCHAR(16) DETERMINISTIC
+  RETURN CONCAT(LPAD(FLOOR(HOUR(SEC_TO_TIME(seconds)) / 24), 2, 0), ' days ',TIME_FORMAT(SEC_TO_TIME(seconds % (24 * 3600)), '%H:%i:%s'));
+  
